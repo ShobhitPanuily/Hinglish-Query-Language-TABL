5980f8f05116c2f1e2083340022f5c444e0c432c
Shobhit Panuily <126354398+ShobhitPanuily@users.noreply.github.com>
2025-06-27 18:54:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,4 +2,5 @@ import os
 
 if __name__ == "__main__":
     port = os.environ.get("PORT", "8501")  
-    os.system(f"streamlit run streamlit_app.py --server.port={port} --server.enableCORS=false")
+    os.system(f"streamlit run streamlit_app.py --server.port={port}")
+    # os.system(f"streamlit run streamlit_app.py --server.port={port} --server.enableCORS=false")
